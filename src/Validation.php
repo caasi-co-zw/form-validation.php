85431eff8d70db389701a4fab831e5d31f9ed2e6
isaac-machakata <isaac@caasi.co.zw>
2022-11-29 21:47:39 +0200

    Improvements

--- src/Validation.php
+++ src/Validation.php
@@ -1,22 +1,27 @@
 <?php
 
-if (!defined('CUSTOM_VALIDATIONS')) {
+if (!defined('CFV_FOLDER')) {
     /**
      * Custom validation folder.
-     * @var string|bool
+     * @var string|null
      */
-    define('CUSTOM_VALIDATIONS', false);
+    define('CFV_FOLDER', null);
 }
 /**
  * Validates your html forms
+ *
+ * Validate your requests parameters with easy to understand code and less code.
  * @author Isaac Machakata <isaac@caasi.co.zw>
  * @link https://github.com/caasi-co-zw/form-validation.php
  * @version 1.0.0
  * @license MIT
+ * @method bool hasErrors()
+ * @method bool isValid()
+ * @method bool validate()
  */
 
 class Validation {
-    const DEFAULTS = [
+    private $validations = [
         Validations::class
     ];
     private $errors = [];
@@ -38,8 +43,33 @@ class Validation {
     public function validate(): bool {
         return true;
     }
+    private function addValidation($class) {
+        $this->validations[] = $class;
+    }
     private function loadValidations() {
+        if (CFV_FOLDER) {
+            // get files
+        }
+    }
+    private function getCustomValidations() {
+        $directory = CFV_FOLDER;
+
+        if (!$directory) {
+            return false;
+        }
+
+        if (is_dir($directory)) {
+            foreach (scandir($directory) as $file) {
+            }
+        }
     }
     private function addError($attribute, $msg = null) {
+        $this->errors[] = [
+            'attribute' => $attribute,
+            'message' => $msg ?? $this->getErrorMsg($attribute)
+        ];
+    }
+    private function getErrorMsg($attribute) {
+        return $this->error_msgs[$attribute] ?? null;
     }
 }
